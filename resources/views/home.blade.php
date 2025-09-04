4ac5d1d062ed4e4ccaf6e7be7574490a928a2112
marlmunar <marlemmmunar@gmail.com>
2025-09-04 12:32:05 +0800

    Added the home page blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body style="padding: 0.5rem;">
+    <h1>Home Page</h1>
+
+    <section style="padding: 1rem; border: 2px solid black; border-radius: 1rem; max-width: 20rem;">
+        <h2>Register as a User</h2>
+        <form action="/register" method="POST" style="display: flex; flex-direction: column; gap: 0.5rem; ">
+            <input type="text" name="name" placeholder="Name">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" value="Register">
+        </form>
+    </section>
+</body>
+</html>
