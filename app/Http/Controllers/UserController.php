96b780976d9b0e2202fdf30d96f71cb6cb3c6b39
marlmunar <marlemmmunar@gmail.com>
2025-09-04 14:19:44 +0800

    Added basic form validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register () {
+    public function register (Request $request) {
+        $incommingFields = $request->validate([
+            'name' => ['required', 'min:4', 'max:25'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8', 'max:15']
+        ]);
         return "Hello from User Contoller";
     }
 }
