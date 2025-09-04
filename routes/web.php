6f17dee53d7ec1fcf0da33144dae6a86929adcd7
marlmunar <marlemmmunar@gmail.com>
2025-09-04 14:12:25 +0800

    Tried sending a response via controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', function () {
-    return "You are now registered";
-});
+Route::post('/register', [UserController::class, 'register']);
